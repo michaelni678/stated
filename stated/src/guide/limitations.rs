54651003db598977e4e593e18686aa8ce6708681
Michael Ni <michael.ni678@gmail.com>
2025-07-30 20:24:42 -0400

    docs: word the limitations docs better

--- stated/src/guide/limitations.rs
+++ stated/src/guide/limitations.rs
@@ -1,12 +1,12 @@
 //! Drawbacks to consider.
 //!
-//! The typestate pattern has several limitations, some of which Stated
-//! addresses.
+//! The typestate pattern has several limitations. Stated helps with some of
+//! them, but not all.
 //!
 //! # Boilerplate
 //!
-//! The typestate pattern has a lot of boilerplate. The whole point of Stated is
-//! to reduce boilerplate!
+//! The typestate pattern has a lot of boilerplate. Stated reduces the amount of
+//! code you'll need to write significantly.
 //!
 //! # Bad Documentation
 //!
@@ -17,15 +17,15 @@
 //! # Increased Compile Times
 //!
 //! The typestate pattern uses a lot of generics, which increases compile time.
-//! Stated does not address this limitation.
+//! Stated does not attempt to decrease compile time.
 //!
 //! # Bad Compiler Errors
 //!
 //! The complex generics used by the typestate pattern often lead to confusing
 //! compiler errors. Stated does not address this limitation.
 //!
-//! # Self Type
+//! # No Self Type
 //!
-//! The typestate pattern doesn't work well with `Self`. `Self` refers to the
-//! current type being implemented, but the generics of this type may be changed
-//! for the typestate pattern to work. Stated does not address this limitation.
+//! The typestate pattern doesn't work well with `Self`. This is due to generic
+//! state changes altering the type that `Self` refers to. Stated does not
+//! attempt to fix this.
