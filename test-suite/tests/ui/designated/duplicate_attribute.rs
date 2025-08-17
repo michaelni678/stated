5167417cca2db64bf822b77de12cfe42c4162f01
Michael Ni <michael.ni678@gmail.com>
2025-08-16 20:18:13 -0400

    test: prevent fmt from putting attrs on different lines in duplicate attr test

--- test-suite/tests/ui/designated/duplicate_attribute.rs
+++ test-suite/tests/ui/designated/duplicate_attribute.rs
@@ -1,9 +1,15 @@
 use stated::stated;
 
 #[stated(states(A))]
+#[rustfmt::skip] // Stops the attributes from going on different lines.
 pub struct Test<#[stated] #[stated] S>;
 
 #[stated]
-impl<#[stated] #[stated] S> Test<S> {}
+impl<
+    #[stated]
+    #[stated] S,
+> Test<S>
+{
+}
 
 fn main() {}
